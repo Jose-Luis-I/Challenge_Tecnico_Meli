f7caa0d3d200ee8ff8ab971294039a834b004163
Jose_Izquierdo <josel405@hotmail.com>
2023-07-11 23:32:33 -0500

    Se agrega las funciones df_items y get_results a la clase info_data

--- info_data.py
+++ info_data.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 
 class info_data():
@@ -18,12 +19,22 @@ class info_data():
         self.cats = requests.get(self.url)
         self.cats_1 = self.cats.json()
         # diccionario de id:categoria dentro del pais con el que se inicializa la clase
-        self.categorias = {el['id'].replace(
-            self.pais, ''): el['name'] for el in self.cats_1}
+        self.categorias = {int(el['id'].replace(
+            self.pais, '')): el['name'] for el in self.cats_1}
         pass
 
-    def items(self, cat_id: str, offset: int):
-        url = f'https://api.mercadolibre.com/sites/{self.pais}/search?category={self.pais+cat_id}&offset={offset}'
+    def items(self, cat_id: int, offset: int):
+        """Devuelve la respuesta directamente del api segun la categoria y el offset"""
+        url = f'https://api.mercadolibre.com/sites/{self.pais}/search?category={self.pais+str(cat_id)}&offset={offset}'
         request = requests.get(url)
         items = request.json()
         return items
+
+    def df_items(self, categoria_id: int, offset: int = 50):
+        """Devuelve un dataframe que contiene los items de la categoria segun el id y el offset"""
+        return pd.DataFrame(self.items(str(categoria_id), offset)['results'])
+
+    def get_results(self, categoria_id: int, n: int):
+        """Devuelve un dataframe con los items de la categoria contenidos en el rango de offset de 0 a n"""
+        ran = range(0, n, 50)
+        return pd.concat([self.df_items(categoria_id, el) for el in ran], ignore_index=True)
